81884906fc90003c3fc542857700f3fe44a34c3c
tomvonheill <tomvonheill@gmail.com>
2018-10-10 10:00:57 -0500

    added map api key and view port meta tags

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoidG9tdm9uaGVpbGwiLCJhIjoiY2puMG12endrNHJkbDNxbnluY2lsdTcyaiJ9.S1xOBR8N-v717P66UCXEfw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
